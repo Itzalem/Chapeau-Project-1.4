d7796a5d36c011cae8b1844e9ae5b7e71a5ad943
Itzalem <melissa.lj04@gmail.com>
2025-05-08 13:30:27 +0200

    Add project files.

---
+++ Chapeau Project 1.4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chapeau_Project_1._4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

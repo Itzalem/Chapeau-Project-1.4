6e7ba0e3a81fa908034969ef6203ae51912de3c9
Thijmenye <729358@student.inholland.nl>
2025-05-28 12:29:50 +0200

    added the form to prepare for payment. the page won't show up though...

---
+++ Chapeau Project 1.4/Models/EPaymentOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Chapeau_Project_1._4.Models
+{
+    public enum EPaymentOptions
+    {
+        DebitCard,
+        CreditCard,
+        Cash
+    }
+}
---
+++ Chapeau Project 1.4/Models/Payment.cs
@@ -0,0 +1,31 @@
+﻿namespace Chapeau_Project_1._4.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public Bill Bill { get; set; }
+        public decimal Tip { get; set; }
+        public decimal Total { get; set; }
+        public int AlcoholDrinks { get; set; }
+        public int NonAlcoholDrinks { get; set; }
+        public EPaymentOptions PaymentType { get; set; }
+        public string Feedback { get; set; }
+
+        public Payment()
+        {
+             
+        }
+
+        public Payment(int paymentId, Bill bill, decimal tip, decimal total, int alcoholDrinks, int nonAlcoholDrinks, EPaymentOptions paymentType, string feedback)
+        {
+            PaymentId = paymentId;
+            Bill = bill;
+            Tip = tip;
+            Total = total;
+            AlcoholDrinks = alcoholDrinks;
+            NonAlcoholDrinks = nonAlcoholDrinks;
+            PaymentType = paymentType;
+            Feedback = feedback;
+        }
+    }
+}

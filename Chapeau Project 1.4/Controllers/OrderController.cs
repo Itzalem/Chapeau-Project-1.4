183e169f07731f8362452bbf7e705e129754f482
Itzalem <melissa.lj04@gmail.com>
2025-06-07 11:38:29 +0200

    user feedback messages changes. success message not showing formating, some messages are double

--- Chapeau Project 1.4/Controllers/OrderController.cs
+++ Chapeau Project 1.4/Controllers/OrderController.cs
@@ -115,7 +115,7 @@ namespace Chapeau_Project_1._4.Controllers
                 }
             }
 
-            TempData["SuccesMessage"] = "Order Sent Successfully";
+            TempData["SuccessMessage"] = "Order Sent Successfully";
 
             return RedirectToAction("TakeOrder", new { orderNumber = order.OrderNumber });
         }
